840fa5ebd0228366a396331b4d1247e671af2538
roboseb <sebastienapril@rocketmail.com>
2024-02-01 16:57:54 -0600

    Add basic functionality for drawing one line at a time.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 -- Vectorizor --
 
 A simple app that will allow creating simple vector drawings and animations. I created this tool to assist with video animations.
+
+
+--- Devlog ---
+
+February 1st, 2024
+
+    Started work on this yesterday, but planning to work on it for another three hours at most today. Should be possible to finish it in that time. 
+
+    Basically the app will immitate a retro style of vector drawing that I'm imagining may have existed, similar to tennis for two. It has a single point that it has to move to draw out lines and create shapes. This app will be a tool to create these animations.
+
+    The app can already save coordinates from clicked positions and redraw the same thing. Now it just needs to animate that.
--- script.js
+++ script.js
@@ -12,8 +12,9 @@ let oldX = canvas.width / 2;
 let oldY = canvas.height / 2;
 
 let moveArray = [
-    { type: 'stroke', x: 0, y: 0, distance:  this.x},
-    { type: 'stroke', x: 50, y: 200, distance: this.x},
+    { type: 'stroke', x: 0, y: 0, distance: Math.hypot(0 - oldX, 0 - oldY)},
+    { type: 'stroke', x: 50, y: 200, distance: Math.hypot(50 - oldX, 200 - oldY)},
+    // { type: 'stroke', x: 400, y: 75, distance: Math.hypot(400 - oldX, 75 - oldY)},
 ];
 
 function draw(x, y) {
@@ -32,7 +33,7 @@ const addMove = (type, x, y) => {
         type: type,
         x: x,
         y: y,
-        distance: Math.hypot(x - oldX, y - oldY),
+        distance: null,
     }
 
     moveArray.push(move);
@@ -56,23 +57,52 @@ animateBtn.addEventListener('click', () => {
 
 });
 
+let currentMove = 0;
+let animatedFrames = 0;
+let drawnLength = 0;
+
 // Start running animation.
 const animate = () => {
-    console.log('animating...');
+
+    // Stop animating once all lines have been drawn.
+    if (animatedFrames == moveArray.length * 60) {
+        console.log('done');
+        return;
+    };
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     oldX = canvas.width / 2;
     oldY = canvas.height / 2;
 
-    ctx.lineDashOffset = 200;
-    ctx.setLineDash([20, 30]);
+    // ctx.lineDashOffset = 500;
+    // ctx.setLineDash([500, 500]);
 
 
-    moveArray.forEach(move => {
-        console.log(move.distance);
+    for (let i = 0; i < currentMove; i++) {
 
-        draw(move.x, move.y)
-    });
+        if (i > moveArray.length - 1) break;
+
+        const lastLineLength = moveArray[i].distance;
+
+        drawnLength += lastLineLength / 60;
+        ctx.setLineDash([lastLineLength, 2000]);
+
+
+        draw(moveArray[i].x, moveArray[i].y)
+    }
+
+    // Draw the next move every second.
+    if (animatedFrames % 60 == 0) {
+        currentMove++;
+    }
+
+    animatedFrames++;
+
+    // Animate a frame 60 times per second.
+
+
+    setTimeout(animate, 1000 / 60);
+
 
 }
 
